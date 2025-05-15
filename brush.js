5eb85fd651834b05a61aeca1e65ec96e13bd150e
Michael Cahana <cahanamichael@gmail.com>
2025-05-15 11:29:20 -0400

    sine wave

--- brush.js
+++ brush.js
@@ -14,7 +14,6 @@ let scaleFactorX, scaleFactorY;
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
-  background("#ffffff");
 
   // Calculate scale factors based on a reference size of 1000x1000
   scaleFactorX = windowWidth / 1000;
@@ -49,41 +48,41 @@ function generatePolygon(c1, c2, c3, t) {
 function draw() {
   const t = frameCount / 30;
 
-  background("#ffe6d4");
+  background("#1B2A4A"); // Dark blue background
 
-  for (let i = 0; i < 10; i++) {
+  const X_OFFSET = -400; // Controls the overall horizontal position
+  const triangle_config = [
+    [-6, -20],
+    [25, -7],
+    [0, 14],
+  ];
+
+  for (let i = 0; i < 30; i++) {
     const multiplier = 30;
+    const waveAmplitude = 50; // Controls the height of the wave
+    const waveFrequency = 0.2; // Controls how many waves appear
+    const yOffset = sin(i * 360 * waveFrequency + t * 2) * waveAmplitude;
 
     randomSeed(33213 * seeds[i]);
-    brush.set("hatch_brush", "#003c32", 1);
+    brush.set("hatch_brush", "#E8B4B8", 2); // Light earthy pink
 
-    brush.setHatch("marker", "#e0b411", 1.1);
+    brush.setHatch("marker", "#D4A5A8", 1); // Darker earthy pink
     brush.hatch(10, 130, 0.8);
 
     brush.polygon(
       generatePolygon(
-        [-6 + i * multiplier, -20 - (i * multiplier) / 2],
-        [25 + i * multiplier, -7 - (i * multiplier) / 2],
-        [0 + i * multiplier, 14 - (i * multiplier) / 2],
-        t
-      )
-    );
-  }
-
-  for (let i = 1; i < 10; i++) {
-    const multiplier = 30;
-
-    randomSeed(33213 * seeds[i]);
-    brush.set("hatch_brush", "#003c32", 1);
-
-    brush.setHatch("marker", "#e0b411", 1.1);
-    brush.hatch(10, 130, 0.8);
-
-    brush.polygon(
-      generatePolygon(
-        [-6 + i * multiplier, -20 + (i * multiplier) / 2],
-        [25 + i * multiplier, -7 + (i * multiplier) / 2],
-        [0 + i * multiplier, 14 + (i * multiplier) / 2],
+        [
+          triangle_config[0][0] + i * multiplier + X_OFFSET,
+          triangle_config[0][1] + yOffset,
+        ],
+        [
+          triangle_config[1][0] + i * multiplier + X_OFFSET,
+          triangle_config[1][1] + yOffset,
+        ],
+        [
+          triangle_config[2][0] + i * multiplier + X_OFFSET,
+          triangle_config[2][1] + yOffset,
+        ],
         t
       )
     );
